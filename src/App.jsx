8ae4f98a0cf1adfc2744c5ad6ffe70c10bfa85d6
IngMario07 <kilimorales_29@hotmail.com>
2026-01-10 15:51:25 -0600

    Actualizacion Analitics1.1

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from 'react'
+import { AnalyticsTracker } from "./AnalyticsTracker";
 import { AppRouter } from './router/AppRouter'
 
 function App() {
   return (
     <>
+    <AnalyticsTracker />
       <AppRouter />
     </>
   )
