8f283c17d8ebd635c7936ded0448025a9e3ec71d
IngMario07 <kilimorales_29@hotmail.com>
2026-01-30 11:44:04 -0600

    Actualizacion Analitics1.13

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -42,7 +42,7 @@ export const Inicio = () => {
         style={{ position: "fixed", top: 0, left: 0, width: "100%", height: "100%", zIndex: -1 }}
         options={{
           preset: "fire",
-          background: { color: "#1a202c" },
+          background: { color: "#1a202c00" },
           fullScreen: { enable: false },
         }}
       />
